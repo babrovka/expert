c44766b8350e8e1b68962bed268c106685a1429e
finewine <finewine@server.(none)>
2009-09-11 15:01:08 +0400

    ok

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "expert"
 
-ssh_options[:port] = 8586
-ssh_options[:username] = "root"
+ssh_options[:port] = 8587
+ssh_options[:username] = "kukhl"
 ssh_options[:forward_agent] = true
 
 set :user, "kukhl"
@@ -28,9 +28,24 @@ set :branch, "master"
 set :scm_passphrase, "gegrby"
  
 
-role :app, "209.20.90.25"
-role :web, "209.20.90.25"
-role :db,  "209.20.90.25", :primary => true
+role :app, "74.207.226.126"
+role :web, "74.207.226.126"
+role :db,  "74.207.226.126", :primary => true
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
 
 
 task :fix_script_perms do
