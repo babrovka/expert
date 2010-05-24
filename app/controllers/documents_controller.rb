43abf41b2467566f6a03a8ef975b3bffe8ff9f6c
finewine <finewine@server.(none)>
2010-05-24 16:48:25 +0400

    ok

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,23 +1,22 @@
 class DocumentsController < ApplicationController
 
-  before_filter :require_user  
+  before_filter :require_user
 
   def download
      doc=Document.find(params[:id])
      if doc.message.order.user==current_user || current_user.admin?
         #Set the X-Accel-Redirect header with the path relative to the /downloads location in nginx
-        response.headers['X-Accel-Redirect'] = "/downloads/#{doc.file.url}"
-	#Set the Content-Type header as nginx won't change it and Rails will send text/html
-	response.headers['Content-Type'] = 'application/octet-stream'
-	#If you want to force download, set the Content-Disposition header (which nginx won't change)
-	response.headers['Content-Disposition'] = "attachment; filename=#{doc.file_file_name}"
-	#Make sure we don't render anything
-	render :nothing => true
+        response.headers['X-Accel-Redirect'] = "/#{doc.file.url}"
+      	#Set the Content-Type header as nginx won't change it and Rails will send text/html
+	      response.headers['Content-Type'] = 'application/octet-stream'
+      	#If you want to force download, set the Content-Disposition header (which nginx won't change)
+      	response.headers['Content-Disposition'] = "attachment; filename=#{doc.file_file_name}"
+      	render :nothing => true
      else
        flash[:notice]="Документ не доступен"
        redirect_to :back
      end
-
   end
 
 end
+
