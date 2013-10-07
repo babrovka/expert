9d8f99641312e337fac5f059517d91403b26821b
Viacheslav Bobrov <babrovka@gmail.com>
2013-10-07 13:09:12 +0400

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,21 +27,28 @@ end
 
 namespace(:thin) do
   task :stop do
-    run "thin stop -C /etc/thin/kzs.yml"
+    run "thin stop -C /etc/thin/#{application}.yml"
    end
   
   task :start do
-    run "thin start -C /etc/thin/kzs.yml"
+    run "thin start -C /etc/thin/#{application}.yml"
   end
 
   task :restart do
-    run "thin restart -C /etc/thin/kzs.yml"
+    run "thin restart -C /etc/thin/#{application}.yml"
   end
 end
 
 namespace :deploy do
-  task :setup_solr_data_dir do
-    run "mkdir -p #{shared_path}/solr/data"
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
   end
 end
 
