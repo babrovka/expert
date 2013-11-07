92491f3ffc60435955ebc8f665bd78a3a1c6e05d
Viacheslav Bobrov <babrovka@gmail.com>
2013-11-07 18:22:03 +0400

    final fixes

--- app/models/order.rb
+++ app/models/order.rb
@@ -16,7 +16,7 @@ class Order < ActiveRecord::Base
   validates_presence_of :status
 
   def paid_for?
-     price && price>0 && price<=payments.sum(:sum) || price == 0
+     price && price>0 && price<=payments.sum(:sum) || order_payment.try(:status) == 2
   end
 
   def with_empty_message
